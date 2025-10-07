1e0ae5f2a16358c3c2d00f2c2cfc309c3c98fe24
navee <navee@NAVEEN>
2025-10-07 15:53:03 +0530

    commit

---
+++ assingment/src/com/tnsif/assignmentone/Main.java
@@ -0,0 +1,12 @@
+
+package com.tnsif.assignmentone;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Commission cm = new Commission();
+		System.out.println(cm.toString());
+	}
+
+}
